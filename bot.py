2010b5e349da2bf09f940ecf4c58d6f118727eeb
aaronangxz <angxze@gmail.com>
2021-02-24 18:11:53 +0800

    Integrated Google Maps Places API

--- bot.py
+++ bot.py
@@ -17,17 +17,24 @@ from telegram.ext import (
     ConversationHandler,
     CallbackContext,
 )
+from googleplaces import GooglePlaces, types, lang
+googleAPI = 'AIzaSyDi8qSI76P9HJRfQKOwP1ju5x9rXzZiJZg'
+google_places = GooglePlaces(googleAPI)
+prevlocation = None
+prevrequest = None
+
 PORT = int(os.environ.get('PORT', 5000))
 TOKEN = os.environ["TOKEN"]
 bot = telegram.Bot(token=TOKEN)
 TYPESPEED = [0.25,0.5,0.75,1]
+QUERYDELAY = [0,1,2,3,4,5,6,7,8,9]
 
 # Enable logging
 logging.basicConfig(filename='bot.log',format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-MYSTATE, ACTION, PHOTO, LOCATION, BIO = range(5)
+MYSTATE, ACTION, USERLOCATION= range(3)
 
 def start(update: Update, context: CallbackContext) -> int:
     giflink = 'https://media.giphy.com/media/12uXi1GXBibALC/giphy.gif'
@@ -66,7 +73,7 @@ def start(update: Update, context: CallbackContext) -> int:
 
 def ideas(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
-    reply_keyboard = [['Central', 'East','West']]
+    reply_keyboard = [['Central', 'East','West','Near me']]
     logger.info("%s needs ideas", user.first_name)
 
     context.bot.sendChatAction(chat_id=update.message.chat_id, action = telegram.ChatAction.TYPING)
@@ -77,6 +84,107 @@ def ideas(update: Update, context: CallbackContext) -> int:
         reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
     )
     return ACTION
+
+# def getLocation(update, user_data):
+#     msg = update.message
+#     user_data['msg'] = msg
+#     user_data['id'] = update.update_id
+#     update.message.reply_text('lat: {}, lng: {}'.format(
+#         msg.location.latitude, msg.location.longitude))
+
+def maprequest(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text('Where are you?\n'
+                                 '(Type the name of the nearest mall / mrt)\n\n'
+                                 'I am still dumb so try to give me more details!\n'
+                                 'For example: "Vivo City" will yield better results than "Vivo"')
+    # global prevrequest 
+    
+    return USERLOCATION
+
+def maprequest_again(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text('Where are you?\n'
+                                 '(Type the name of the nearest mall / mrt)\n\n'
+                                 'I am still dumb so try to give me more details!\n\n'
+                                 'For example: "313 Somerset" will yield better results than "313"')
+    return USERLOCATION
+
+def mapquery(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("%s queries map", user.first_name)
+    reply_keyboard = [['Nice!', 'Nahh']]
+
+    global prevlocation 
+    global prevrequest 
+
+    # prevrequest = update.message.text
+    # logger.info("prevrequest is %s",prevrequest)
+    
+    # prevlocation =  update.message.text
+
+    # logger.info("prevlocation is %s",prevlocation)
+    # context.bot.sendChatAction(chat_id=update.message.chat_id, action = telegram.ChatAction.TYPING)
+    # sleep(random.choice(TYPESPEED))
+
+    # userquery = MessageHandler(Filters.text)
+    #sleep(random.choice(TYPESPEED))
+    # query_result = ''
+    # global prevlocation
+    # if prevlocation is None:
+    #     prevlocation = update.message.text
+    if prevlocation == None:
+        prevlocation = update.message.text
+        prevrequest = prevlocation
+        logger.info("prevlocation is %s",prevlocation)
+    elif prevlocation == 'Nahh' or prevlocation == 'Near Me': 
+        prevlocation = prevrequest
+        logger.info("prevlocation is %s",prevlocation)
+    elif prevlocation == '/exit':
+        return ConversationHandler.END
+    logger.info("prevlocation is %s",prevlocation)
+
+
+
+    query_result = google_places.nearby_search(
+        location= (prevlocation), keyword= 'restaurant',
+        radius= 100, types=[types.TYPE_FOOD,types.TYPE_RESTAURANT,types.TYPE_CAFE])
+    logger.info("%s searched %s", user.first_name,update.message.text)
+    logger.info("query results are: %s",query_result.places)
+
+    update.message.reply_text('Okay here you go 😬')
+    
+    # delaytime = random.choice(QUERYDELAY)
+    delaytime = random.randint(0,len(query_result.places)-1)
+    logger.info("delaytime is %f",delaytime)
+    #update.message.reply_text('delaytime is ' + delaytime)
+
+    # places = query_result.places[delaytime]
+
+    logger.info("query result: %s",query_result.places[delaytime].name)
+
+    for place in query_result.places:        
+        #update.message.reply_text('count is ' + count)
+        # if count == delaytime:
+        query_result.places[delaytime].get_details()
+        update.message.reply_text('Want to try ' + query_result.places[delaytime].name + ' ? 🤔\n'
+                                    + query_result.places[delaytime].url)
+        # context.bot.sendLocation(update.message.chat_id, latitude= place.geo_location['lat'], longitude=place.geo_location['lng'])
+        context.bot.sendLocation(update.message.chat_id, latitude= query_result.places[delaytime].geo_location['lat'], longitude=query_result.places[delaytime].geo_location['lng'])
+        for photo in query_result.places[delaytime].photos:
+            photo.get(maxheight=500, maxwidth=500)
+            # query_result.places[delaytime].photo.get(maxheight=500, maxwidth=500)
+            context.bot.sendPhoto(chat_id=update.message.chat_id,photo = photo.url)
+            break
+
+        context.bot.sendChatAction(chat_id=update.message.chat_id, action = telegram.ChatAction.TYPING)
+        sleep(random.choice(TYPESPEED))
+        update.message.reply_text(
+                'Can ah?',
+                reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True),
+            )
+        return ACTION
+
+
+
 
 def Nah(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
@@ -98,7 +206,7 @@ def Nah(update: Update, context: CallbackContext) -> int:
         reply_markup=ReplyKeyboardRemove(),
     )
     update.message.reply_text(
-        'Bye!',
+        'Bye😗',
         reply_markup=ReplyKeyboardRemove(),
     )
     return ConversationHandler.END
@@ -242,23 +350,42 @@ def ending(update: Update, context: CallbackContext) -> int:
         'Enjoy your meal!😋😋😋',
         reply_markup=ReplyKeyboardRemove()
     )
+    global prevrequest
+    global prevlocation
+    prevrequest = None
+    prevlocation = None
     return ConversationHandler.END
 
 def exit(update: Update, context: CallbackContext) -> int:
     user = update.message.from_user
     logger.info("User %s canceled the conversation.", user.first_name)
-    
+
+    # context.bot.sendLocation(update.message.chat_id, latitude=1.290188, longitude=103.851563),
+
     context.bot.sendChatAction(chat_id=update.message.chat_id, action = telegram.ChatAction.TYPING)
     sleep(random.choice(TYPESPEED))
 
     update.message.reply_text(
-        'Bye! Chat me again next time ❤️', reply_markup=ReplyKeyboardRemove()
+        'Bye! Chat with me again next time ❤️', reply_markup=ReplyKeyboardRemove()
     )
     return ConversationHandler.END
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text('Hello! Welcome to Hungry Leh. Patrick will try his best to help you decide what to eat.\n\n'
+                                '🍔Commands\n'
+                                '/start to start asking\n'
+                                '/random to generate random places (Under Construction)\n'
+                                '/help to access help menu\n'
+                                '/exit to quit\n\n'
+                                '🍣Features\n'
+                                'Patrick will look for random restaurants within 100m radius.\n'
+                                'He will then send you the restaurant name, google maps link and pictures!\n'
+                                'Feel free to hurt his feelings by rejecting his suggestions and ask for a new one.\n\n'
+                                '🍺Future Developments\n'
+                                '- To choose cuisine / price range\n'
+                                '- Suggest places based on overall ratings\n\n'
+                                'By @zzeexxuuaann')
 
 def echo(update, context):
     """Echo the user message."""
@@ -287,9 +414,14 @@ def main():
             ACTION: [MessageHandler(Filters.regex('^Central$'), places_central), 
                     MessageHandler(Filters.regex('^East$'), places_east),
                     MessageHandler(Filters.regex('^West$'), places_west),
+                    MessageHandler(Filters.regex('^Near me$'), maprequest),
                     MessageHandler(Filters.regex('^YES PLS$'), places_random),
                     MessageHandler(Filters.regex('^Lmao lame$'), ideas),
-                    MessageHandler(Filters.regex('^Nice!$'), ending)]
+                    MessageHandler(Filters.regex('^Nahh$'), mapquery),
+                    MessageHandler(Filters.regex('^Nice!$'), ending),
+                    MessageHandler(Filters.text, mapquery)],
+            USERLOCATION: [MessageHandler(Filters.text,mapquery)
+                    ],
         },
         fallbacks=[CommandHandler('exit', exit)],
     )
@@ -297,10 +429,11 @@ def main():
     dp.add_handler(conv_handler)
     # on different commands - answer in Telegram
     #dp.add_handler(CommandHandler("start", start))
-    #dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
+    #dp.add_handler(MessageHandler(Filters.text, echo))
+    #dp.add_handler(MessageHandler(Filters.text, mapquery))
 
     # log all errors
     dp.add_error_handler(error)
@@ -308,14 +441,14 @@ def main():
     #updater.bot.deleteWebhook()
 
     # Start the Bot
-    # updater.start_polling()
+    updater.start_polling()
 
     # Start the Bot
     updater.start_webhook(listen="0.0.0.0",
                           port = PORT,
                           url_path= TOKEN)
     # updater.bot.set_webhook(url=settings.WEBHOOK_URL)
-    updater.bot.set_webhook('https://hungrylehbot.herokuapp.com/' + TOKEN)
+    # updater.bot.set_webhook('https://hungrylehbot.herokuapp.com/' + TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
